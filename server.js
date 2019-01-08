97db9d6285cb4f17fd070ab41a6bda15a2cb7383
evg1n <evginserbest@outlook.com>
2019-01-08 21:47:06 +0300

    last commit before 252

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 const app = express();
+const PORT = 3000;
+
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const db = {
     users: [
@@ -23,8 +27,17 @@ const db = {
             entries: 0,
             joined: new Date()
         }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '$2a$10$nw8uDRsqiGOLh1fzp2le4enaKw300qJZEVs16MaDEvhoDvUKwLGyC',
+            email: ''
+        }
     ]
 }
+
+
 
 app.get('/', (req, res) => {
     res.send(db.users);
@@ -33,7 +46,7 @@ app.get('/', (req, res) => {
 app.post('/signin', (req, res) => {
     if (req.body.email === db.users[0].email && req.body.password === db.users[0].password) {
         res.json('success')
-
+        console.log('success logging in')
     } else {
     res.status(400).json('error logging in');
     }
@@ -41,11 +54,13 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    bcrypt.hash(password, null, null, (err, hash) => {
+        console.log(hash);
+    })
     db.users.push( {
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date()
     })
@@ -53,18 +68,37 @@ app.post('/register', (req, res) => {
     res.json(db.users[db.users.length - 1]);
 })
 
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    
+    let found = false;
+    db.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    });
 
+    if (!found) {
+        res.status(400).json('not found!');
+    }
+})
 
-app.listen(3000, () => {
-    console.log('app is listening to port 3000');
+app.post('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    db.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    });
+    if (!found) {
+        res.status(400).json('not found!');
+    }
+})
+
+app.listen(PORT, () => {
+    console.log('app is listening to port:', PORT);
 } );
-
-
-/*
-
-/
-/signin
-/register
-/profile/:userid
-/image
-*/
